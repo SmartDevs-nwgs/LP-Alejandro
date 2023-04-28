750aff27c00ca25d4aacb8c2c1f134b60313a071
Axel <axeellima@gmail.com>
2023-04-28 03:22:36 -0300

    add: responsive style carousel

---
+++ src/Components/ListCarousel/index.jsx
@@ -0,0 +1,198 @@
+import {
+  BsCloudRainFill,
+  BsFillHeartFill,
+  BsFillHeartbreakFill,
+} from 'react-icons/bs'
+import { SiSpringCreators, SiHeadspace } from 'react-icons/si'
+import { MdNoDrinks } from 'react-icons/md'
+import { FaUsers } from 'react-icons/fa'
+import { GiPunch } from 'react-icons/gi'
+
+const ListCarousel = ({ widthSize }) => {
+  return (
+    <>
+      {widthSize < 700 ? (
+        <>
+          <div className='card-one card card-active'>
+            <div>
+              <BsCloudRainFill className='cloud' />
+              <h2>Depresión</h2>
+            </div>
+          </div>
+          <div className='card-two card'>
+            <div>
+              <SiSpringCreators className='anxiety' />
+              <h2>Ansiedad</h2>
+            </div>
+          </div>
+          <div className='card-three card'>
+            <div>
+              <SiHeadspace className='stress' />
+              <h2>Estrés Crónico</h2>
+            </div>
+          </div>
+          <div className='card-four card'>
+            <div>
+              <MdNoDrinks className='no-drink' />
+              <h2>Trastorno Obsesivo-Compulsivo</h2>
+            </div>
+          </div>
+          <div className='card-five card'>
+            <div>
+              <FaUsers className='social' />
+              <h2>Habilidades Sociales</h2>
+            </div>
+          </div>
+          <div className='card-six card'>
+            <div>
+              <GiPunch className='punch' />
+              <h2>Agresividad</h2>
+            </div>
+          </div>
+          <div className='card-seven card'>
+            <div>
+              <BsFillHeartFill className='heart' />
+              <h2>Autoestima</h2>
+            </div>
+          </div>
+          <div className='card-eight card'>
+            <div>
+              <BsFillHeartbreakFill className='broken-heart' />
+              <h2>Relaciones afectivas</h2>
+            </div>
+          </div>
+        </>
+      ) : widthSize >= 700 && widthSize < 1000 ? (
+        <>
+          <div className='cards-div1'>
+            <div className='card-one card card-active'>
+              <div>
+                <BsCloudRainFill className='cloud' />
+                <h2>Depresión</h2>
+              </div>
+            </div>
+            <div className='card-two card'>
+              <div>
+                <SiSpringCreators className='anxiety' />
+                <h2>Ansiedad</h2>
+              </div>
+            </div>
+          </div>
+
+          <div className='cards-div2'>
+            <div className='card-three card'>
+              <div>
+                <SiHeadspace className='stress' />
+                <h2>Estrés Crónico</h2>
+              </div>
+            </div>
+            <div className='card-four card'>
+              <div>
+                <MdNoDrinks className='no-drink' />
+                <h2>Trastorno Obsesivo-Compulsivo</h2>
+              </div>
+            </div>
+          </div>
+
+          <div className='cards-div3'>
+            <div className='card-five card'>
+              <div>
+                <FaUsers className='social' />
+                <h2>Habilidades Sociales</h2>
+              </div>
+            </div>
+            <div className='card-six card'>
+              <div>
+                <GiPunch className='punch' />
+                <h2>Agresividad</h2>
+              </div>
+            </div>
+          </div>
+
+          <div className='cards-div4'>
+            <div className='card-seven card'>
+              <div>
+                <BsFillHeartFill className='heart' />
+                <h2>Autoestima</h2>
+              </div>
+            </div>
+            <div className='card-eight card'>
+              <div>
+                <BsFillHeartbreakFill className='broken-heart' />
+                <h2>Relaciones afectivas</h2>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className='cards-div1'>
+            <div className='card-one card card-active'>
+              <div>
+                <BsCloudRainFill className='cloud' />
+                <h2>Depresión</h2>
+              </div>
+            </div>
+            <div className='card-two card'>
+              <div>
+                <SiSpringCreators className='anxiety' />
+                <h2>Ansiedad</h2>
+              </div>
+            </div>
+
+            <div className='card-three card'>
+              <div>
+                <SiHeadspace className='stress' />
+                <h2>Estrés Crónico</h2>
+              </div>
+            </div>
+          </div>
+
+          <div className='cards-div2'>
+            <div className='card-four card'>
+              <div>
+                <MdNoDrinks className='no-drink' />
+                <h2>Trastorno Obsesivo-Compulsivo</h2>
+              </div>
+            </div>
+
+            <div className='card-five card'>
+              <div>
+                <FaUsers className='social' />
+                <h2>Habilidades Sociales</h2>
+              </div>
+            </div>
+            <div className='card-six card'>
+              <div>
+                <GiPunch className='punch' />
+                <h2>Agresividad</h2>
+              </div>
+            </div>
+          </div>
+
+          <div className='cards-div3'>
+            <div className='card-seven card'>
+              <div>
+                <BsFillHeartFill className='heart' />
+                <h2>Autoestima</h2>
+              </div>
+            </div>
+            <div className='card-eight card'>
+              <div>
+                <BsFillHeartbreakFill className='broken-heart' />
+                <h2>Relaciones afectivas</h2>
+              </div>
+            </div>
+            <div className='card-one-repeat card '>
+              <div>
+                <BsCloudRainFill className='cloud' />
+                <h2>Depresión</h2>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  )
+}
+export default ListCarousel
